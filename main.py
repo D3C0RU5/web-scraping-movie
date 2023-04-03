3d18db1f2ef1a587a782c9991b37a09f6e69926e
D3C0RU5 <chpeterle.profissional@gmail.com>
2023-04-03 09:54:39 -0300

    feat: improve arguments

--- main.py
+++ main.py
@@ -1,8 +1,169 @@
 #!/usr/bin/env python
+import logging
+import json
+import multiprocessing
 import sys
+import time
 from services.scrap import WebScraper
+from multiprocessing import Process
 
-movie_name_args = ' '.join(sys.argv[1:])
-image_link = WebScraper().get_image(movie_name_args)
 
-print(image_link)
+def get_json_object():
+    with open('data/movies.json', 'r') as json_file:
+        json_object = json.load(json_file)
+        json_file.close()
+
+    return json_object
+
+
+def get_url_movies(list_movies: list[str]):
+
+    for index, movie in enumerate(list_movies):
+        len_movies = len(list_movies)
+        logging.info(f"[{index+1}/{len_movies}] Scraping [{movie['name']}]")
+        url = WebScraper().get_image(movie['name'])
+        movie['url'] = url
+
+    return list_movies
+
+
+def refresh_file_with_processed_images(content):
+    with open('data/movies.json', 'w') as json_file:
+        json_file.write(content)
+        json_file.close()
+
+
+def config():
+    logging.basicConfig(
+        format='%(levelname)s: %(message)s',
+        encoding='utf-8',
+        level=logging.DEBUG,
+    )
+
+
+def get_args():
+    """
+    Arguments definitions:
+
+    -r | --range : define a range to scrap movies, by default the range is all movies
+    ex: -r 0 100
+
+    -a | --async : define quantity of asyncronous requests, by default is 5
+    ex: -a 10
+
+    -c | --change : change scrap already executed, by default this option is disabled, 0 is disabled and 1 is enabled
+    ex: -c 0 | -c 1
+
+    """
+    args = sys.argv[1:]
+
+    args_definition = {
+        '--change': 0,
+        '--async': 5,
+    }
+
+    for index, arg in enumerate(args):
+        if arg == '-r' or arg == '--range':
+            range = args[index+1:index+3]
+
+            args_definition['--range'] = {
+                "start": int(range[0]),
+                "end": int(range[1]),
+            }
+        elif arg == '-c' or arg == '--change':
+            args_definition['--change'] = int(args[index+1])
+        elif arg == '-a' or arg == '--async':
+            args_definition['--async'] = int(args[index+1])
+
+    return args_definition
+
+
+def get_movie_range(args, movies):
+    if '--range' in args.keys():
+        return movies[args['--range']['start']: args['--range']['end']]
+
+    return movies
+
+
+def filter_with_change_arg(args, movies):
+    if args['--change'] == 0:
+        return [movie for movie in movies if 'url' not in movie.keys()]
+
+    return movies
+
+
+def start_scraping(args):
+    json_object = get_json_object()
+
+    movies = get_movie_range(args, json_object['movies'])
+
+    movies = filter_with_change_arg(args, movies)
+
+    movies = perform_requests(args, movies)
+
+    print(movies)
+
+
+def divide_chunks(args, movies):
+    chunk_size = args['--async']
+    chunks = []
+    for i in range(0, len(movies), chunk_size):
+        chunks.append(movies[i:i + chunk_size])
+    return chunks
+
+
+def worker(movie, return_dict):
+    logging.info(f"Scraping {movie['name']}")
+    movie['url'] = WebScraper().get_image(movie['name'])
+    logging.info(f"Scraping {movie['name']} completed successfully")
+
+    return_dict[movie['name']] = movie['url']
+
+
+def execute_chunk(chunk, movies):
+    manager = multiprocessing.Manager()
+    return_dict = manager.dict()
+    jobs = []
+    for movie in chunk:
+        p = multiprocessing.Process(target=worker, args=(movie, return_dict))
+        jobs.append(p)
+        p.start()
+
+    for proc in jobs:
+        proc.join()
+
+    return return_dict
+
+
+def perform_requests(args, movies):
+    movie_chunks = divide_chunks(args, movies)
+
+    results = []
+    for chunk in movie_chunks:
+        chunk_results = execute_chunk(chunk, movies)
+        for result in chunk_results:
+            movie = {
+                "name": result,
+                "url": chunk_results[result],
+            }
+            results.append(movie)
+
+    print(results)
+
+# movies_to_text = json.dumps(movies, ensure_ascii=False)
+# refresh_file_with_processed_images(movies_to_text)
+
+# logging.info(f"Scrapping completed.")
+
+
+if __name__ == '__main__':
+    try:
+        args = get_args()
+        config()
+        start_scraping(args)
+
+    except KeyboardInterrupt as exc:
+        logging.info(f"Scrapping stoped.")
+
+    except Exception as exc:
+        logging.exception(f"Something goes wrong: %s" % exc)
--- services/scrap.py
+++ services/scrap.py
@@ -3,31 +3,32 @@ import urllib
 from urllib.request import Request, urlopen
 from Levenshtein import distance
 
-headers ={
+headers = {
     'User-Agent': "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/111.0",
-    'content-type':'text/html; charset=utf-8',
+    'content-type': 'text/html; charset=utf-8',
     'server': 'server'
 }
+
 
 class WebScraper:
     def get_image(self, movie_name):
         url = urllib.parse.quote(movie_name)
-        print(f"Iniciando scraping do filme: {movie_name}")
+        # print(f"Iniciando scraping do filme: {movie_name}")
 
         req = Request(
-            url=f'https://www.imdb.com/find/?q={url}', 
+            url=f'https://www.imdb.com/find/?q={url}',
             headers=headers
         )
-        print(f"Buscando resultados...")
+        # print(f"Buscando resultados...")
         webpage = urlopen(req).read()
         soup = BeautifulSoup(webpage, "html.parser")
 
         # Obtendo lista de resultados
-        print(f"Listando resultados...")
+        # print(f"Listando resultados...")
         movie_list = soup.find_all(class_="find-title-result")
 
         # Encontrando filme na lista
-        print(f"Calculando distancia de items para o nome [{movie_name}]")
+        # print(f"Calculando distancia de items para o nome [{movie_name}]")
 
         selected_movie_index = 0
         less_distance_value = None
@@ -35,34 +36,35 @@ class WebScraper:
         for index, movie_item in enumerate(movie_list):
             movie_item_name = movie_item.find("a").text
             distance_value = distance(movie_name, movie_item_name)
-            print(f"[{index}] -- {movie_item_name}")
-            print(f"Result -> {distance_value}")
+            # print(f"[{index}] -- {movie_item_name}")
+            # print(f"Result -> {distance_value}")
 
-            if(less_distance_value == None or distance_value < less_distance_value):
+            if (less_distance_value == None or distance_value < less_distance_value):
                 less_distance_value = distance_value
                 selected_movie_index = index
 
         # Selecionando melhor resultado
         selected_movie = movie_list[selected_movie_index]
         selected_movie_name = selected_movie.find("a").text
-        print(f"O melhor resultado foi: {selected_movie_name}")
+        # print(f"O melhor resultado foi: {selected_movie_name}")
 
         # Buscando página do filme
-        print(f"Buscando página do filme...")
+        # print(f"Buscando página do filme...")
         movie_link = "https://www.imdb.com/" + selected_movie.find("a")["href"]
         req = Request(
-            url=movie_link, 
+            url=movie_link,
             headers=headers
         )
         webpage = urlopen(req).read()
         soup = BeautifulSoup(webpage, "html.parser")
 
-        image_poster_link = soup.find(class_="hero-media__watchlist").findNext('a')['href']
+        image_poster_link = soup.find(
+            class_="hero-media__watchlist").findNext('a')['href']
 
         # Buscando poster
-        print(f"Buscando poster do filme...")
+        # print(f"Buscando poster do filme...")
         req = Request(
-            url="https://www.imdb.com/" + image_poster_link, 
+            url="https://www.imdb.com/" + image_poster_link,
             headers=headers
         )
         webpage = urlopen(req).read()
@@ -70,5 +72,5 @@ class WebScraper:
 
         image_poster_link = soup.findAll('img')[1]['src']
 
-        print(f"Link encontrado!!")
+        # print(f"Link encontrado!!")
         return image_poster_link
